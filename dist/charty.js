cf0f86a3deae043d8127e16852625e58ad2c3e7f
Marcio Caraballo <marcio.caraballososa@gmail.com>
2013-09-05 01:31:45 -0300

    Triangle chart draws two triangles per data value

--- dist/charty.js
+++ dist/charty.js
@@ -1909,19 +1909,20 @@ Triangle drawer.
     define('charty/triangle',[
       'd3.chart',
       'charty/chartynames',
+      'underscore',
       'charty/simpledatagroup'
       ],
-      function (d3, Charty) {
+      function (d3, Charty, _) {
         /** Export global even in AMD case in case this script
         is loaded with others */
-        return factory(d3, Charty);
+        return factory(d3, Charty, _);
     });
   }
   else {
     /** Browser globals */
-    factory(d3, Charty);
+    factory(d3, Charty, _);
   }
-}(this, function (d3, Charty) {
+}(this, function (d3, Charty, _) {
   d3.chart(Charty.CHART_NAMES.SIMPLE_DATA_GROUP)
     .extend(Charty.CHART_NAMES.TRIANGLE, {
     /**
@@ -1960,7 +1961,7 @@ Triangle drawer.
           chart.c = (d.c || defaults.c);
           chart.clickEvent = d.clickEvent;
 
-          return this.selectAll('path').data(d.data);
+          return this.selectAll('path').data(d);
 
         },
         /**
@@ -1975,15 +1976,13 @@ Triangle drawer.
         events : {
           'merge' : function(){
 
-            var chart = this.chart(),
-                y1 = chart.yscale.map(0),
-                band = chart.xscale.band(1);
+            var chart = this.chart();
 
             this.attr('class', function(d){
                   return (d.c || chart.c);
                 })
                 .attr('d', function(d){
-                  return chart.getPath(d, y1, band);
+                  return chart.getPath(d);
                 });
 
             if (chart.clickEvent){
@@ -2012,42 +2011,39 @@ Triangle drawer.
     @param {Object} d
     @return {String} path
     */
-    getPath : function(d, y1, band){
+    /*getPath : function(d, y1, band){
 
       var x1 = this.xscale.map(d.x,1);
 
       return  ('M ' + x1 + ' ' + y1 +
               ' L ' + (x1 + band/2) + ' ' + this.yscale.map(d.y) +
               ' L ' + (x1 + band) + ' ' + y1);
-    }
+    }*/
     /**
-    Triangle drawer IS SimpleDataInput
-    However, transform must be redefined in order to
+    Transform must be redefined in order to
     separate a triangle in two constituting parts
 
     @method
     @param {Object} data Data Acccessor
     @return {Object} already mapped values for each datapoint
-
+    */
     transform : function(data){
-      var result = [];
+      var result = [],
+          dataArray = data.next().data,
+          self = this,
+          xBand = this.xscale.band(1),
+          zeroY = this.yscale.map(0);
 
-      var dataArray = data.next().data;
+      _.each(dataArray, function (element){
+        var x1 = self.xscale.map(element.x,1),
+            x2 = x1 + (xBand/2),
+            x3 = x1 + xBand,
+            y1 = zeroY,
+            y2 = self.yscale.map(element.y);
 
-      for(var i = 0; i < dataArray.length; i++){
-        var element = dataArray[i];
-
-        var x1 = this.xscale.map(element.x,1);
-        var x2 = x1 + this.xscale.band(1)/2;
-        var x3 = x1 + this.xscale.band(1);
-
-        var y1 = this.yscale.map(0);
-        var y2 = this.yscale.map(element.y);
-
-        result.unshift({x1 : x1, y1: y1, x2 : x2, y2 : y2, x3 : x2, y3 : y1, c: element.c1});
-        result.unshift({x1 : x2, y1: y1, x2 : x2, y2 : y2, x3 : x3, y3 : y1, c: element.c2});
-
-      }
+          result.unshift({x1 : x1, y1: y1, x2 : x2, y2 : y2, x3 : x2, y3 : y1, c: element.c1});
+          result.unshift({x1 : x2, y1: y1, x2 : x2, y2 : y2, x3 : x3, y3 : y1, c: element.c2});
+      });
       return result;
     },
     /**
@@ -2055,13 +2051,12 @@ Triangle drawer.
     data, visualized as dots.
 
     @method
-    @param {Object} d
+    @param {Object} d Data point
     @return {String} path
-
+    */
     getPath : function(d){
-      var p = 'M '+d.x1+' '+d.y1+' L '+d.x2+' '+d.y2+' L '+d.x3+' '+d.y3;
-      return p;
-    }*/
+      return ('M '+ d.x1 + ' ' + d.y1 + ' L ' + d.x2 +' ' + d.y2 + ' L '+ d.x3 + ' ' + d.y3);
+    }
   });
 }));
 /**
